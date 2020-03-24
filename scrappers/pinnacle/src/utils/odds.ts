1caeb6d588eebc73d06b6c8c5cbfd3f2f3fb5b2a
Lucca Mordente <luccamordente@gmail.com>
2020-03-24 10:23:52 -0300

    Move Pinnacle scrapper to it's own service

---
+++ scrappers/pinnacle/src/utils/odds.ts
@@ -0,0 +1,13 @@
+function americanToDecimal(american: number): number {
+  let ret: number;
+  if (american < 0) {
+    ret = 1 + -100 / american;
+  } else {
+    ret = 1 + american / 100;
+  }
+  return ret;
+}
+
+export {
+  americanToDecimal
+};
