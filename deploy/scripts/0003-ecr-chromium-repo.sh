974434daba5bac8e4980395ba79b37d0b7e372e9
Gabriel Cangussu <gabrielcangussu@gmail.com>
2020-04-14 18:05:34 -0300

    bet365 k8s

--- deploy/scripts/0003-ecr-chromium-repo.sh
+++ deploy/scripts/0003-ecr-chromium-repo.sh
@@ -1,18 +1,19 @@
 # Create the repository to put our main image
 
 aws ecr create-repository \
-  --repository-name poluga \
+  --region ap-south-1 \
+  --repository-name poluga-chromium \
   --image-tag-mutability IMMUTABLE \
   --image-scanning-configuration scanOnPush=true
 
 # Output:
 # {
 #     "repository": {
-#         "repositoryArn": "arn:aws:ecr:us-east-1:914977074407:repository/poluga",
+#         "repositoryArn": "arn:aws:ecr:ap-south-1:914977074407:repository/poluga-chromium",
 #         "registryId": "914977074407",
-#         "repositoryName": "poluga",
-#         "repositoryUri": "914977074407.dkr.ecr.us-east-1.amazonaws.com/poluga",
-#         "createdAt": 1586298791.0,
+#         "repositoryName": "poluga-chromium",
+#         "repositoryUri": "914977074407.dkr.ecr.ap-south-1.amazonaws.com/poluga-chromium",
+#         "createdAt": 1586897311.0,
 #         "imageTagMutability": "IMMUTABLE",
 #         "imageScanningConfiguration": {
 #             "scanOnPush": true
